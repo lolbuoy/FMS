a0a1265b70f5061b6d6cd0e516a5e0d71b365e60
Abhinava Teja Reddy A <abhinavatejareddya0509@gmail.com>
2024-07-16 23:58:46 +0530

    Update App.svelte

--- App.svelte
+++ App.svelte
@@ -1,57 +1,100 @@
 <script>
-    // Define state variables for the display boxes
-    let display1 = "Display Box 1";
-    let display2 = "Display Box 2";
-    let display3 = "Display Box 3";
+  import io from 'socket.io-client';
+  import { onMount } from "svelte"
+
+  // Define state variables for the display boxes
+  let display1 = "";
+  let display2 = "";
+  let display3 = "";
+  let flight_name = "";
+  let id = "";
+
+  let flights = []
+
+  onMount(() => {
+    localStorage.debug = "*"
+    const socket = io('http://100.75.51.60:4010'); // Change the URL to your socket server
+
+    // Connect to the socket server
+
+    // Listen for socket events
+    socket.on('redis_update', (data) => {
+      console.log("Received redis update")
+      console.log(data)
   
-    // Action for Button 1
-    function handleButton1Click() {
-      display1 = "Button 1 was clicked!";
-      display2 = "Updated by Button 1";
-      display3 = "Updated by Button 1";
-    }
+      let lat = ""
+      let lon = ""
+      let alt = ""
+
+      const socket_data = Object.entries(data)
   
-    // Action for Button 2
-    function handleButton2Click() {
-      display1 = "Button 2 was clicked!";
-      display2 = "Updated by Button 2";
-      display3 = "Updated by Button 2";
-    }
-  </script>
+      for (const [flight_id, flight_data] of socket_data) { 
+        // flights.push({
+        //   flight_id,
+        //   ...flight_data,
+        // })
+        if (flight_id == "Bullet_FOXTROT") {
+          flight_name = flight_id
+          lat = flight_data.lat
+          lon = flight_data.lon
+          alt = flight_data.alt
+          id = flight_data.flt_id
+        }
+      }
+
+      display1 = lat
+      display2 = lon
+      display3 = alt
+    });
+  })
   
-  <style>
-    .container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 20px;
-      margin-top: 50px;
-    }
-  
-    .buttons {
-      display: flex;
-      gap: 10px;
-    }
-  
-    .display-box {
-      width: 200px;
-      height: 100px;
-      border: 1px solid #ccc;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: #f9f9f9;
-      font-size: 1.2em;
-    }
-  </style>
-  
-  <div class="container">
-    <div class="buttons">
-      <button on:click={handleButton1Click}>Button 1</button>
-      <button on:click={handleButton2Click}>Button 2</button>
-    </div>
-    <div class="display-box">{display1}</div>
-    <div class="display-box">{display2}</div>
-    <div class="display-box">{display3}</div>
-  </div>
-  
+
+  // Action for Arm button
+  // function handleArmClick() {
+  //   socket.emit('arm');
+  // }
+
+  // Action for Disarm button
+  // function handleDisarmClick() {
+  //   socket.emit('disarm');
+  // }
+</script>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    margin-top: 50px;
+  }
+
+  /* .buttons {
+    display: flex;
+    gap: 10px;
+  } */
+
+  .display-box {
+    width: 200px;
+    height: 100px;
+    border: 1px solid #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: #f9f9f9;
+    font-size: 1.2em;
+  }
+</style>
+
+<div class="container">
+  <!-- <div class="buttons">
+    <button on:click={handleArmClick}>Arm</button>
+    <button on:click={handleDisarmClick}>Disarm</button>
+  </div> -->
+  <div class="display-box">{"FLIGHT NAME: "+flight_name}</div>
+  <div class="display-box">{"FLIGHT ID: "+id}</div>
+  <div class="display-box">{"LAT: "+display1}</div>
+  <div class="display-box">{"LONG: "+display2}</div>
+  <div class="display-box">{"ALT: "+display3}</div>
+</div>
